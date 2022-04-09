30672ee2bd1c4d345f7f861b071d7dbda90823c9
Rojj <tserodgers@gmail.con>
2022-04-08 18:04:09 -0700

    printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with propergrammar ,
+ * but the outcome is a piece of art
+ * Return: 0 if exited, otherwise non-zero
+ */
+int main(void)
+{
+	printf("with prper grammar , but the 
+			outcome is a piece of art,\n")
+		return	(0)
+}
