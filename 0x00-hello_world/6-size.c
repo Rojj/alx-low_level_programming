9e6873193a79aa8c4ee567fceb428dac38c902a3
Rojj <tserodgers@gmail.con>
2022-04-10 06:04:01 -0700

    size dit c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the sizes of various types on the computer
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(long unsigned int));
+	printf("size of an int: %ld byte(s)\n", sizeof(long unsigned int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long unsigned int));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(long unsigned int));
+	printf("size of a float: %ld byte(s)\n", sizeof(long unsigned int));
+	return	(0);
+	}
