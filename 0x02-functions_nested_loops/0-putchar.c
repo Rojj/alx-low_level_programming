d6f76112c0e0ad612d890b46f640a7b2dfa64d74
Rojj <tserodgers@gmail.con>
2022-04-17 06:06:07 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,15 @@
 
 /**
  * main - prints _putchar
+ *
  * description - prints _putchar
+ *
  * Return: 0
  */
 
 int main(void)
 {
-	char text(9) = "_putchar";
+	char text[9] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
