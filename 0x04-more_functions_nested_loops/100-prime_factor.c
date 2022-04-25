62c596d5e9e4bb49ad8a6bba809b8714aa2493d0
Rojj <tserodgers@gmail.con>
2022-04-25 16:31:06 -0700

    prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,13 +7,17 @@
 
 int main(void)
 {
-	unsigned long int i = 3, n = 612852475143;
+	long n, i;
 
-	for (; i < 12057; i += 2)
+	n = 612852475143;
+	for (i = 2; i < n; i++)
 	{
-		while (n % 1 == 0 && n != i)
-			n /= i;
+		while (n % i == 0)
+			n = n / i;
 	}
+
 	printf("%lu\n", n);
+
 	return	(0);
 }
+
